8b550b0f963f697a73d3384516ab1572a86dc483
hakeem mohammed <hk.mo.rd98@gmail.com>
2023-06-06 18:56:27 +0300

    weather-app

---
+++ src/components/api.js
@@ -0,0 +1,11 @@
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo/places';
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'a991e9bd92msh1be7b73d90ae0a5p12b9a1jsn509803f336dd',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5'
+export const WEATHER_API_KEY = '6d446885eaf6a48bad7fef55b0def010';
